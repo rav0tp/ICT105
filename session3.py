84f41dc0948a4a3dc98b7f27e723860a55594329
rav0tp <12045104@stanleycollege.edu.au>
2026-04-14 09:07:08 +0800

    Update session3.py

--- session3.py
+++ session3.py
@@ -1,11 +1,43 @@
-session3
+session3 # type: ignore
 
-# Define a and b.
+courses = ["Physics I", "Calculus I", "Biology I", "History I", "Microeconomics"]
 
-a = 5
+print(courses)
+print(sorted(courses))
+print(sorted(courses, reverse=True))
 
-b = 3
+courses.reverse()
+print(courses)
+courses.reverse()
+print(courses)
 
-myout = (a+b) # Add a + b and store to myout.
+courses.sort()
+print(courses)
+courses.sort(reverse=True)
+print(courses)
 
-print("The sum of a + b equals to", myout) # Send to standard out.
+# Replace a course
+courses[0] = "Chemistry I"
+print("Withdrawn: Physics I")
+print("Added: Chemistry I")
+print(courses)
+
+# Add 3 courses
+courses.insert(0, "Calculus II")
+courses.insert(2, "English I")
+courses.append("Psychology I")
+print(courses)
+
+# Remove 4 with pop()
+print("Removed:", courses.pop(0))
+print("Removed:", courses.pop(1))
+print("Removed:", courses.pop(2))
+print("Removed:", courses.pop(3))
+print(courses)
+
+# Tuples
+tuples = [(1,"Physics I"),(2,"Calculus I"),(3,"Biology I")]
+names = []
+for t in tuples:
+    names.append(t[1])
+print(names)
