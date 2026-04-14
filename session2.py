6512724d208876152390db275e03379f1164168f
rav0tp <12045104@stanleycollege.edu.au>
2026-04-14 08:44:42 +0800

    Update session2.py

--- session2.py
+++ session2.py
@@ -1,25 +1,23 @@
 # Variables
 a = 15
 b = 12
+
 print(type(a))
 print(type(b))
 
-# Arithmetic
-print("a + b =", a + b)
-print("a - b =", a - b)
-print("a * b =", a * b)
-print("a / b =", a / b)
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
 
-# Type casting
 c = int(a / b)
 print(c, type(c))
+
 c = float(c)
 print(c, type(c))
 
-# Strings
 message = "The result of a divided by b is?"
 print(message + " " + str(c))
 
-# Comparison
 print(a > b)
 print(a == b)
